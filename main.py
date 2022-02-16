a6411e8ccdaed20d5f002bb8b2e85d90f1ae8a9c
mariya tikhonova <koshkinoko.hana@gmail.com>
2022-02-16 15:11:26 +0300

    sapper finished

--- main.py
+++ main.py
@@ -1,5 +1,73 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from sapper import init_sapper
+from telegram.ext import CallbackContext, Updater, CommandHandler, MessageHandler, Filters, BaseFilter
 
-print(init_sapper('1'))
+from sapper import init_sapper, answer, clear
+import telegram
+from telegram import Update, ForceReply
+
+token = '5281554471:AAFzRHww-Y3PvZ1Ji-B-XiHbTBPbqKnPWNI'
+bot = telegram.Bot(token)
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    update.message.reply_html(
+        'Привет! Чтобы начать игру, введи /game. Чтобы узнать правила, введи /rules'
+    )
+
+
+def rules(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    update.message.reply_html(
+        'В игре есть поле 5 на 5. Там спрятаны 10 мин. '
+        'У вас есть 15 ходов, чтобы найти как можно больше мин. Очки между играми суммируются. '
+        'для каждого хода введите букву(строка) и число(колонка). '
+        'Можно вводить несколько ходов одновременно, разделяя их запятыми.'
+        'Наберите как можно больше очков, чтобы попасть в рейтинг!'
+    )
+
+
+def game(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    update.message.reply_html(
+        init_sapper(user.id)
+    )
+
+
+def process_answer(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    update.message.reply_html(
+        answer(user.id, update.message.text)
+    )
+
+
+
+def clear_process(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    clear(user.id)
+    update.message.reply_html(
+        'Достижения обнулены! у вас 0 очков.'
+    )
+
+
+def main() -> None:
+    updater = Updater(token)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("game", game))
+    dispatcher.add_handler(CommandHandler("clear", clear_process))
+    dispatcher.add_handler(CommandHandler("rules", rules))
+    dispatcher.add_handler(MessageHandler(Filters.regex('([a-eA-E][1-5][, ]*){1,10}'), process_answer))
+    dispatcher.add_handler(MessageHandler(Filters.all, start))
+
+    # Start the Bot
+    updater.start_polling()
+    updater.idle()
+
+
+main()
--- sapper.py
+++ sapper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import random
+import copy
 
 games = {}
 
@@ -10,19 +11,97 @@ sapper_field = '''🔵🔵🔵🔵🔵
 🔵🔵🔵🔵🔵
 🔵🔵🔵🔵🔵'''
 
+row_names = ['a', 'b', 'c', 'd', 'e']
+
+empty_answer = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
 
 def generate():
-    return random.sample(range(25), 10)
+    ans_list = random.sample(range(25), 10)
+    res = copy.deepcopy(empty_answer)
+    for i in ans_list:
+        res[i // 5][i % 5] = 1
+    print('Generated answer: ')
+    print(ans_list)
+    print(res)
+    return res
 
 
 def init_sapper(user_id):
     new_game = generate()
-    games.update({user_id: new_game})
-    return sapper_field
+    games.get(user_id)
+    total = 0
+    if user_id in games:
+        total = games.get(user_id)['total']
+    games.update({
+        user_id: {
+            'game': new_game,
+            'touched_count': 0,
+            'total': total,
+            'answer': copy.deepcopy(empty_answer)
+        }
+    })
+    print('Game for user ' + str(user_id))
+    field = '<CODE>_ 1 2 3 4 5\n'
+    rows = sapper_field.split('\n')
+    for i in range(len(rows)):
+        field += row_names[i] + rows[i] + '\n'
+    field += '</CODE>'
+    return field
+
+
+def reformat_answer(user_id, ans):
+    game = games.get(user_id)
+    answer_list = ans.split(',')
+    dif = game['touched_count'] + len(answer_list) - 15
+    if dif > 0:
+        game['touched_count'] = 15
+    else:
+        game['touched_count'] += len(answer_list)
+
+    for a in range(len(answer_list) - (dif if dif > 0 else 0)):
+        item = answer_list[a].strip().lower()
+        i = row_names.index(item[0])
+        j = int(item[1]) - 1
+        game['answer'][i][j] = 1
+
+        if game['game'][i][j] == 1:
+            game['total'] += 1
+
+    print('User answered: ')
+    print(ans)
+    print(game['answer'])
+
+
+def clear(user_id):
+    game = games.get(user_id)
+    if game:
+        game['total'] = 0
 
 
 def answer(user_id, ans):
     game = games.get(user_id)
-    gamer_field = sapper_field
+    if not game:
+        return 'Привет! Чтобы начать игру, введи /game .'
+    res_field = '<CODE>_ 1 2 3 4 5\n'
 
-    return sapper_field
+    reformat_answer(user_id, ans)
+    final = game['touched_count'] == 15
+    for i in range(5):
+        res_field += row_names[i]
+        for j in range(5):
+            if game['answer'][i][j] and game['game'][i][j]:
+                res_field += '🟢'
+            elif game['answer'][i][j]:
+                res_field += '⚪️'
+            elif final and game['game'][i][j]:
+                res_field += '🔴'
+            else:
+                res_field += '🔵'
+        res_field += '\n'
+    res_field += '</CODE>'
+    before = 'Ходы закончились! Финальное поле:\n' if final \
+        else 'осталось {0} ходов:\n'.format(15 - game['touched_count'])
+    after = 'у вас {0} очков.'.format(game['total'])
+    if game['touched_count'] == 15:
+        after += 'Чтобы начать новую игру, введите /game . Чтобы обнулить достижения, введите /clear .'
+    return before + res_field + after
